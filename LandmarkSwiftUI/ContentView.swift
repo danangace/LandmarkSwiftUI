d5db4c531aced8f7c2f8c0feb17ccf8873a1e6a1
danangace <danangbahari89@gmail.com>
2020-09-19 18:18:21 +0700

    Initial Commit

---
+++ LandmarkSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandmarkSwiftUI
+//
+//  Created by Danang Putra Bahari on 19/09/20.
+//  Copyright © 2020 Danang Putra Bahari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
